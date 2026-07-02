7f78c43921f5fe016d015706747f2a9342f43ff2
Maximiliano Morra <morramaximiliano25@gmail.com>
2026-07-02 19:27:22 -0300

    cambios en libs/sequelize quitamos env locales

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,13 +1,7 @@
 import { Sequelize } from 'sequelize';
-
-import { config } from '../env-config/config.js';
 import setupModels from '../db/models/index.js';
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
-const sequelize = new Sequelize(URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
