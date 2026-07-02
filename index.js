6a46f9c0258d82558a51450a246b06fdde81026f
Maximiliano Morra <morramaximiliano25@gmail.com>
2026-07-02 19:49:17 -0300

    ajustamos problemas con CORS...

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://fraganciasuy-backend.onrender.com',
-      'https://fraganciasuy-frontend.vercel.app',
-    ],
+    origin: ['https://fraganciasuy-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
